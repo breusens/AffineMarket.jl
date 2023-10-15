f9f1cc362c36fae2fecc2aec28252e7ce2f9c67b
breusens <Brent.reusens@btinternet.com>
2023-10-15 10:37:14 +0100

    add modular

--- src/AffineMarket.jl
+++ src/AffineMarket.jl
@@ -1,11 +1,17 @@
 module AffineMarket
-export MarketDynamics,MarketState,Observations,ObservationsFromState!,UpdateState!,ExpectedObservations!
+export AffineDynamics,AffineState
 #include("VectorStoredArray.jl")
-using VectorStoredArray: StoredNode, Storage, StoredValue
+using Batcher
+using MarketData
+using Random
+import MarketData: Observations
+import MarketModels: MarketDynamics,MarketState,ObservationsFromState!,UpdateState!,ExpectedObservations! 
+ 
 using FloatingNumberType
 
 
-struct MarketDynamics
+struct AffineDynamics<: MarketDynamics
+    MarketFile::String
     NF::Int64
     Initialrate::SimType
     k1::SimType
@@ -18,70 +24,66 @@ struct MarketDynamics
     se2::SimType
 end
 
-struct OneCurrency <: StoredNode
-    X0::StoredValue
-    X::StoredValue
+struct OneCurrency <: MarketState
+    X0::Batched
+    X::Batched
 end
 
-struct MarketState <: StoredNode
+struct AffineState <: MarketState
     USD::OneCurrency
 end
 
-struct OneCurrencyObs <: StoredNode
-    X0::StoredValue
-    X::StoredValue
-    ON::StoredValue
+
+function MarketState(t::Batched,CV::CA,Dynamics::AffineDynamics)
+    X=CV(Dynamics.Initialrate*ones(Dynamics.NF-1))
+    X0=CV(Dynamics.Initialrate)
+    OCM=OneCurrency(X0,X)
+    return AffineState(OCM)
 end
 
-struct Observations <: StoredNode
-    USD::OneCurrencyObs
-end
-
-function MarketState(t::SimType,S::Storage,Dynamics::MarketDynamics)
-    X=Dynamics.Initialrate*ones(Dynamics.NF-1)
-    X0=Dynamics.Initialrate
-    OCM=OneCurrency(S,X0,X)
-    return MarketState(S,OCM)
-end
-
-function Observations(t::SimType,S::Storage,State::MarketState,MD::MarketDynamics)
-    X0=State.USD.X0
-    X=State.USD.X                                                                                                                                                                                                                                        
-    ON=State.USD.X[end]+0.0005*randn()
-    OCM=OneCurrencyObs(S,X0,X,ON)
-    return Observations(S,OCM)
-end
-
-function ObservationsFromState!(t::SimType,State::MarketState,out::Observations,MD::MarketDynamics)
-    out.USD.X0=State.USD.X0
-    out.USD.X=State.USD.X                                                                                                                                                                                                                                        
-    out.USD.ON=State.USD.X[end]+0.0005*randn()
-    return nothing
-end
-
-function UpdateState!(t0::SimType,t1::SimType,State::MarketState,NextState::MarketState,MD::MarketDynamics)
-    kappa=[x for x in 1:(MD.NF-1)]
-    kappa=MD.k1*((MD.b).^kappa)
-    DT=(t1-t0)/360
+function Observations(t::Batched,CV::CA,State::AffineState,MD::AffineDynamics)
     X0=State.USD.X0
     X=State.USD.X
-    drift=[X0;X]
-    X0+=[MD.s0*log(SimType(1.0)+exp(MD.b0*only(X0)))*SimType(randn())*sqrt(DT)]
-    drift=-kappa.*diff(drift)*DT
-    X+=drift+MD.s1*randn(MD.NF-1)*sqrt(DT)
-    NextState.USD.X0=X0
-    NextState.USD.X=X
+    Noise=CV(SimType,(),randn)                                                                                                                                                                                                                                      
+    ON=State.USD.X[end]+SimType(0.0005)*Noise
+    OCM=OneCurrencyObs(X0,X,ON)
+    return ObservedMarket(OCM)
+end
+
+function ObservationsFromState!(t::Batched,State::AffineState,out::ObservedMarket,MD::AffineDynamics,CV::CA)
+    out.USD.X0.=State.USD.X0
+    out.USD.X.=State.USD.X
+    Noise=CV(SimType,(),randn)                                                                                                                                                                                                                                      
+    out.USD.ON.=State.USD.X[end]+SimType(0.0005)*Noise
     return nothing
 end
-function ExpectedObservations!(t1::SimType,t2::SimType,State::MarketState,out::Observations,MD::MarketDynamics)
+
+function UpdateState!(t0::Batched,t1::Batched,State::AffineState,NextState::AffineState,MD::AffineDynamics,CV::CA)
     kappa=[x for x in 1:(MD.NF-1)]
     kappa=MD.k1*((MD.b).^kappa)
-    DT=(t2-t1)/360
-    out.USD.X0=State.USD.X0
+    DT=(t1-t0)/SimType(360)
+    X0=State.USD.X0
+    X=State.USD.X
+    drift0=[X0;X]
+    Noise=CV(SimType,(),randn)
+    X0+=MD.s0*log(SimType(1.0)+exp(MD.b0*X0))*Noise*sqrt(DT)
+    drift=-kappa.*diff(drift0).*DT
+    Noise2=CV(SimType,(MD.NF-1,),randn)
+    X.+=drift.+MD.s1.*Noise2.*sqrt(DT)
+    NextState.USD.X0.=X0
+    NextState.USD.X.=X
+    return nothing
+end
+
+function ExpectedObservations!(t1::Batched,t2::Batched,State::AffineState,out::ObservedMarket,MD::AffineDynamics,CV::CA)
+    kappa=[x for x in 1:(MD.NF-1)]
+    kappa=MD.k1*((MD.b).^kappa)
+    DT=(t2-t1)/SimType(360)
+    out.USD.X0.=State.USD.X0
     drift=[State.USD.X0;State.USD.X]
-    drift=-kappa.*diff(drift)*DT
-    out.USD.X=State.USD.X+drift                                                                                                                                                                                                                                    
-    out.USD.ON=out.USD.X[end]
+    d=-kappa.*diff(drift).*DT
+    out.USD.X.=State.USD.X.+d                                                                                                                                                                                                                                  
+    out.USD.ON.=out.USD.X[end]
     return nothing
 end
 
